be3181f2ba33f51f3548fef9d71f805310806f0b
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2024-02-08 21:44:23 -0500

    Add files via upload

---
+++ Labs/Lab 8/src/OneFrameView.java
@@ -0,0 +1,50 @@
+package cp213;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+/**
+ * Puts five right triangle views into the same JPanel using various layouts.
+ *
+ * @author Ryan Chisholm
+ * @author David Brown from Byron Weber-Becker
+ * @version 2023-11-28
+ */
+@SuppressWarnings("serial")
+public class OneFrameView extends JPanel {
+    // The model views.
+    private RTButtonView bView = null;
+    private RTGraphicView gView = null;
+    private RTNumericView nView = null;
+    private RTSameView sView = null;
+    private RTSliderView slView = null;
+
+    /**
+     * View constructor.
+     *
+     * @param model The model to attach the individual views to.
+     */
+    public OneFrameView(final RTModel model) {
+	this.nView = new RTNumericView(model);
+	this.bView = new RTButtonView(model);
+	this.gView = new RTGraphicView(model);
+	this.sView = new RTSameView(model);
+	this.slView = new RTSliderView(model);
+	this.layoutView();
+    }
+
+    /**
+     * Lays out the individual model views within the main frame.
+     */
+    private void layoutView() {
+	// Place the numeric and button views on top and the graphic view
+	// underneath.
+	this.setLayout(new BorderLayout());
+	this.add(this.gView, BorderLayout.CENTER);
+	this.add(this.nView, BorderLayout.WEST);
+	this.add(this.sView, BorderLayout.SOUTH);
+	this.add(this.slView, BorderLayout.EAST);
+	this.add(this.bView, BorderLayout.NORTH);
+    }
+}
